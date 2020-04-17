2e06094a7ed83cbeec5f0f2fb5d6c3d5c7124877
cferdinandi <chris@gomakethings.com>
2020-04-17 19:17:22 -0400

    Updated

--- functions/random.js
+++ functions/random.js
@@ -1,23 +1,17 @@
 const friendlyWords = require("friendly-words");
 
+const Reader = require('@maxmind/geoip2-node').Reader;
+
 exports.handler = function(event, context, callback) {
-	const { predicates, objects } = friendlyWords;
-	const numberOfPredicates = predicates.length;
-	const numbersOfObjects = objects.length;
-
-	const randomPredicate =
-		predicates[Math.floor(Math.random() * numberOfPredicates)];
-	const randomObject = objects[Math.floor(Math.random() * numbersOfObjects)];
-
-	const output = `${randomPredicate}-${randomObject}`;
-
-	callback(null, {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
-			'Access-Control-Allow-Headers': '*'
-		},
-		statusCode: 200,
-		body: output
+	Reader.open('/usr/local/share/GeoIP/GeoIP2-Country.mmdb').then(reader => {
+	  callback(null, {
+	  	headers: {
+	  		'Access-Control-Allow-Origin': '*',
+	  		'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+	  		'Access-Control-Allow-Headers': '*'
+	  	},
+	  	statusCode: 200,
+	  	body: reader.country()
+	  });
 	});
 };
